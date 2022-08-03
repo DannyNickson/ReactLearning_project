f6eb50387640aaf16661dcf5f47c0026028d81e9
anny <puchkovdd@gmail.com>
2022-08-03 15:11:58 +0300

    added new UI components to the project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './styles/App.css';
 import PostItems from "./components/PostItems";
 import { useState } from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 // import Counter from "./components/counter";
 
 function App() {
@@ -12,23 +14,16 @@ function App() {
     { id: 2, title: 'Javascript', body: "Description 2" },
     { id: 3, title: 'Javascript', body: "Description 3" },
   ])
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: "Description 1" },
-    { id: 2, title: 'Python', body: "Description 2" },
-    { id: 3, title: 'Python', body: "Description 3" },
-  ]) 
-  const [posts3, setPosts3] = useState([
-    { id: 1, title: 'TypeScript', body: "Description 1" },
-    { id: 2, title: 'TypeScript', body: "Description 2" },
-    { id: 3, title: 'TypeScript', body: "Description 3" },
-  ])
 
 
   return (
     <div className="App">
-      <PostList posts={posts} title={"Посты про JS"} />
-      <PostList posts={posts2} title={"Посты про Python"} />
-      <PostList posts={posts3} title={"Посты про TypeScript"} />
+      <form action="">
+        <MyInput type="text" placeholder="Название поста" />
+        <MyInput type="text" placeholder="Описание поста"/>
+        <MyButton>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts}  title={"Посты про JS"} />
     </div>
   );
 }
