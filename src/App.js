4584f8675b180bfdad1459d0d370403828f99c8e
anny <puchkovdd@gmail.com>
2022-08-03 20:49:42 +0300

    added delete button, callback function, postform component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItems from "./components/PostItems";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 // import Counter from "./components/counter";
 
 function App() {
@@ -15,15 +16,27 @@ function App() {
     { id: 3, title: 'Javascript', body: "Description 3" },
   ])
 
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+
 
   return (
     <div className="App">
-      <form action="">
-        <MyInput type="text" placeholder="Название поста" />
-        <MyInput type="text" placeholder="Описание поста"/>
-        <MyButton>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts}  title={"Посты про JS"} />
+      <PostForm create={createPost} />
+      {posts.length !== 0
+        ?
+        <PostList remove={removePost} posts={posts} title={"Посты про JS"} />
+        :
+        <h1 style={{ textAlign: 'center' }}>
+          Посты не были найденны
+        </h1>
+      }
     </div>
   );
 }
