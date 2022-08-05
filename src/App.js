825c90ef2b1a4affb84437ae31978933f3520d24
anny <puchkovdd@gmail.com>
2022-08-05 14:19:27 +0300

    added customs hooks

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './styles/App.css';
 import { useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import { useMemo } from "react";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePost";
 
 
 function App() {
@@ -17,17 +17,8 @@ function App() {
   ])
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal,setModal] = useState(false)
-  //Важное описание работы сортировки. Метод sort() не возвращает новый массив а мутирует старый. Так как нам нельз изменять на прямую разворачиваем новый массив - копию оригинального и его мутируем.
-  const sortedPostst = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sorterAndSearchedPosts = useMemo(() => {
-    return sortedPostst.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPostst])
+  
+  const sorterAndSearchedPosts = usePosts(posts,filter.sort,filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -36,10 +27,6 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
-
-
-
-
 
   return (
     <div className="App">
